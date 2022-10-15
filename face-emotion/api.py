5898745421a55bc37f130a916ab6907f84204a05
narenb7 <hinarenb@gmail.com>
2022-10-15 15:31:30 -0400

    changes to api.py

--- face-emotion/api.py
+++ face-emotion/api.py
@@ -11,7 +11,7 @@ triton_url = 'triton-docker:8000'
 # We instantiate a face-emotion detector with the location of the pretrained models.
 # In this case, the model needs to be set from our emotions.py
 face_model_path = './haarcascade_frontalface_default.xml'
-model = Sentiment(face_model_path, triton_url='triton:8001'))
+model = Sentiment(face_model_path, triton_url)
 
 # Let's generate a new FastAPI app
 # Generate a FastAPI instance called `app` with the title 'Face-Emotion'
